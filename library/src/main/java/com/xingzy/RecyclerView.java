004442fab843d5439c1edc7af195346bd6c2e86f
xingzy <xingzy@yxt.com>
2019-03-07 17:43:32 +0800

    init

--- library/src/main/java/com/xingzy/RecyclerView.java
+++ library/src/main/java/com/xingzy/RecyclerView.java
@@ -14,8 +14,30 @@ import java.util.List;
 public class RecyclerView extends ViewGroup {
 
     //缓存已经加载到屏幕上的view
-    List<View> viewList;
+    private List<View> viewList;
+    //记录在Y轴上滑动的距离
+    private int currentY;
+    //记录在RecyclerView加载的总数据
+    private int rowCount;
+    //记录在屏幕中的第一个View在数据内
+    private int firstRow;
+    //持有一个回收池的引用
+    private Recycler recycler;
+    //recyclerview中第一个View的左上顶点离屏幕的距离
+    private int scrollY;
+    //初始化 onLayout第一次
+    private boolean needRelayout;
+    //当前recyclerview的宽度
+    private int width;
+    //当前recyclerview的高度
+    private int height;
 
+    //item 高度
+    private int[] heights;
+    //最小滑动距离
+    private int touchSlop;
+
+    Adapter adapter;
 
     public RecyclerView(Context context) {
         super(context);
@@ -32,5 +54,18 @@ public class RecyclerView extends ViewGroup {
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
 
+    }
+
+    interface Adapter {
+
+        int getItemViewType(int row);
+
+        int getViewTypeCount();
+
+        View onCreateViewHolder(int position, View convertView, ViewGroup parent);
+
+        View onBinderViewHolder(int position, View convertView, ViewGroup parent);
+
+        int getHeight(int index);
     }
 }
